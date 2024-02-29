3883e8b60fadc3fc669ee843c876c08890c6add5
Nirupama-Prasad-0987 <nirupama.prasad@capgemini.com>
2024-02-29 17:13:48 +0530

    first-commit

---
+++ Ticket.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Diagnostics.Metrics;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace ProjectStructure
+{
+    internal class Ticket
+{
+    [Key]
+    public int TicketId { get; set; }
+
+    [Required(ErrorMessage = "Title is required")]
+    [StringLength(50, ErrorMessage = "Title cannot exceed 50 characters")]
+    public string Title { get; set; }
+
+    [StringLength(250, ErrorMessage = "Description cannot exceed 250 characters")]
+    public string Description { get; set; }
+
+    [ForeignKey("Customer")]
+    [JsonIgnore]
+    public Customer RaisedBy { get; set; }
+
+    public TicketStatus Status { get; set; } = TicketStatus.Pending;
+
+    [JsonIgnore]
+    public ServiceAgent AssignedTo { get; set; }
+
+    [StringLength(50)]
+    public string Category { get; set; }
+
+    [Required(ErrorMessage = "CreatedDate is required")]
+    public DateTime CreatedDate { get; set; }
+}
+}
---
+++ User.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion.Internal;
+
+namespace ProjectStructure
+{
+    public abstract class User
+    {
+        [Key]
+        public UInt32 UserId { get; set; }
+
+        [Required(ErrorMessage = "Username is required")]
+        [StringLength(50, MinimumLength = 3, ErrorMessage = "Username must be between 3 and 50 characters")]
+        public string UserName { get; set; }
+
+        [Required(ErrorMessage = "Email is required")]
+        [EmailAddress(ErrorMessage = "Invalid email format")]
+        public string UserEmail { get; set; }
+
+        [Required(ErrorMessage = "Password is required")]
+        [RegularExpression("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[^\\da-zA-Z]).{8,}$", ErrorMessage = "Password must be at least 8 characters long and contain at least one uppercase letter, one lowercase letter, one digit, and one special character")]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "Phone number is required")]
+        [RegularExpression(@"^\d{10}$", ErrorMessage = "Phone number must be 10 digits")]
+        public string Phone { get; set; }
+
+
+
+
+
+    }
+}
